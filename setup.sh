2b0205db3b5c90eb4cb04e58a3defb6038f12cc9
Lin Yang <github@linyang.me>
2021-05-07 22:21:08 -0700

    Auto switch my repos to ssh protocol

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ set -o nounset
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 find $ROOT_DIR/*/ -type f -iname setup.sh -exec '{}' \;
+
+find $HOME/*/.git -type f -iname config -exec sed -i 's/https:\/\/github.com\/lyang\//git@github.com:lyang\//' {} \;
