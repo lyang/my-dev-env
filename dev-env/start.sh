7dbec774873e644ad525322d1ef709ded5a714a4
Lin Yang <github@linyang.me>
2021-05-30 21:26:41 -0700

    Refactoring start script

--- dev-env/start.sh
+++ dev-env/start.sh
@@ -9,6 +9,9 @@ docker build --pull -t linyang1218/dev-env:lyang $ROOT_DIR
 ARGS="-it --env DEV_ENV=true --env TERM=$TERM --env TZ=$TZ --name dev-env-$TIMESTAMP"
 
 if [ -z "$DEV_ENV" ]; then
-  ARGS="$ARGS --volume /var/run/docker.sock:/var/run/docker.sock --volume $SSH_AUTH_SOCK:$SSH_AUTH_SOCK --env SSH_AUTH_SOCK --volume $(gpgconf --list-dir agent-extra-socket):/home/gnupg/S.gpg-agent"
+  DOCKER_SOCK="--volume /var/run/docker.sock:/var/run/docker.sock"
+  GPG_SOCK="--volume $(gpgconf --list-dir agent-extra-socket):/home/gnupg/S.gpg-agent"
+  SSH_SOCK="--volume $SSH_AUTH_SOCK:$SSH_AUTH_SOCK --env SSH_AUTH_SOCK"
+  ARGS="$ARGS $DOCKER_SOCK $GPG_SOCK $SSH_SOCK"
 fi
 docker run $ARGS linyang1218/dev-env:lyang $@
