3992e0018218002e547244b06fa9797dbb349c48
Lin Yang <github@linyang.me>
2021-11-18 14:42:17 -0800

    Add pre-commit and poetry to python env

--- pyenv/config.sh
+++ pyenv/config.sh
@@ -2,4 +2,9 @@
 
 pyenv install --skip-existing 3.9.7 && pyenv global 3.9.7
 pip install --upgrade pip
-pip install pynvim pyright
+pip install pre-commit pynvim pyright
+
+if [ "$(command -v poetry)" = "" ]; then
+  curl -sSL https://install.python-poetry.org | python -
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.zshenv
+fi
