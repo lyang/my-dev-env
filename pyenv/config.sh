a98c8bdaee5ecb543da2b479c2764f5624c9d3e0
Lin Yang <github@linyang.me>
2021-11-29 09:22:13 -0800

    Add tox to default python packages

--- pyenv/config.sh
+++ pyenv/config.sh
@@ -2,7 +2,7 @@
 
 pyenv install --skip-existing 3.9.7 && pyenv global 3.9.7
 pip install --upgrade pip
-pip install pre-commit pynvim pyright
+pip install pre-commit pynvim pyright tox
 
 if [ "$(command -v poetry)" = "" ]; then
   curl -sSL https://install.python-poetry.org | python -
