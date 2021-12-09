fa172881d09794545744ced478881c14f20959dc
Lin Yang <github@linyang.me>
2021-12-09 13:22:30 -0800

    Install Idea for all users

--- idea/install.sh
+++ idea/install.sh
@@ -5,13 +5,20 @@ if [ "$(uname)" = "Darwin" ]; then
   IDEA_VERSION=$(brew info --cask intellij-idea-ce | head -n1 | grep -oE '202[0-9]\.[0-9]+')
 else
   IDEA_BUILD=2021.3
-  IDEA_VERSION=$(echo $IDEA_BUILD | grep -oE '202[0-9]\.[0-9]+')
-  IDEA_DIR="$HOME/.local/share/JetBrains/IdeaIC${IDEA_VERSION}"
-  if [ ! -d $IDEA_DIR ]; then
-    curl -fSL https://download.jetbrains.com/idea/ideaIC-$IDEA_BUILD.tar.gz --create-dir -o $IDEA_DIR/installer.tgz
-    tar --strip-components=1 -xzf $IDEA_DIR/installer.tgz -C $IDEA_DIR && rm $IDEA_DIR/installer.tgz
-    chmod -R a+rX $IDEA_DIR/bin/idea.sh
-    mkdir -p $HOME/bin && ln -fs $IDEA_DIR/bin/idea.sh $HOME/bin/idea
+  IDEA_DIR="/opt/idea/$IDEA_BUILD"
+  if [ ! -d "$IDEA_DIR" ]; then
+    sudo sh -c "
+      mkdir -p /opt/idea/bin $IDEA_DIR && chmod -R a+rX /opt/idea
+      curl -sfLo $IDEA_DIR/installer.tgz https://download.jetbrains.com/idea/ideaIC-$IDEA_BUILD.tar.gz
+      tar --strip-components=1 -xzf $IDEA_DIR/installer.tgz -C $IDEA_DIR && rm $IDEA_DIR/installer.tgz
+      ln -fs $IDEA_DIR/bin/idea.sh /opt/idea/bin/idea
+    "
+  fi
+  IDEA_VERSION=$(cat $IDEA_DIR/product-info.json | jq -r .version)
+
+  grep '^export PATH=/opt/idea/bin:' /etc/zsh/zprofile &> /dev/null
+  if [ $? != 0 ]; then
+    sudo sh -c "echo 'export PATH=/opt/idea/bin:\$PATH' >> /etc/zsh/zprofile"
   fi
 fi
 
