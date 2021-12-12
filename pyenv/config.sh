8fd8ef8186b03be7a8eedb3a5e00903dbde39f56
Lin Yang <github@linyang.me>
2021-12-12 00:40:36 -0800

    Update python version/packages

--- pyenv/config.sh
+++ pyenv/config.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
 
-pyenv install --skip-existing 3.9.7 && pyenv global 3.9.7
-pip install --upgrade pip
-pip install podman-compose pre-commit pynvim pyright tox
+VERSIONS=(3.10.1 3.9.9 3.8.12 3.7.12)
+
+for version in $VERSIONS; do
+  pyenv install --skip-existing $version && pyenv local $version
+  pip install --upgrade pip
+  pip install podman-compose pre-commit pynvim pyright tox tox-pyenv
+done
+
+pyenv global ${VERSIONS[1]}
 
 if [ "$(command -v poetry)" = "" ]; then
   curl -sSL https://install.python-poetry.org | python -
