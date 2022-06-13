6ebd1dadd305fba67a2c0ae177e386498f0084b6
Lin Yang <github@linyang.me>
2022-06-13 16:58:21 -0700

    Bump ruby version to 3.1.2

--- rbenv/config.sh
+++ rbenv/config.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 eval "$(rbenv init - zsh)"
-rbenv install --skip-existing 3.0.2 && rbenv global 3.0.2
+rbenv install --skip-existing 3.1.2 && rbenv global 3.1.2
 rbenv rehash && gem install bundler neovim solargraph
