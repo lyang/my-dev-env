c334a3cd37536ece6a73157b1826316e7d405022
Lin Yang <github@linyang.me>
2022-07-14 22:54:38 -0700

    Ansible - Migrate Color Scheme

--- nord-terminal/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-if [ $(uname) = "Darwin" ]; then
-  curl -sfLo /tmp/Nord.itermcolors https://raw.githubusercontent.com/arcticicestudio/nord-iterm2/develop/src/xml/Nord.itermcolors
-elif [ -d "$HOME/.nord-gnome-terminal" ]; then
-  git -C $HOME/.nord-gnome-terminal pull
-else
-  git clone https://github.com/arcticicestudio/nord-gnome-terminal.git $HOME/.nord-gnome-terminal
-fi
