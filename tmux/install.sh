e5af920f7b4f50f480b2f56a801f95d9e8f3fc62
Lin Yang <github@linyang.me>
2022-07-15 16:53:57 -0700

    Ansible - Migrate tmux

--- tmux/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-set -o errexit
-
-if [ -d "$HOME/.tmux/plugins/tpm" ]; then
-  git -C $HOME/.tmux/plugins/tpm pull
-else
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
