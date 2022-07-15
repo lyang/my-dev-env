586efc648cb1dcdb00849bb4689c105be6f75b57
Lin Yang <github@linyang.me>
2022-07-14 22:54:38 -0700

    Ansible - Migrate Color Scheme

--- nord-terminal/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ $(uname) = "Darwin" ]; then
-  open -a iterm /tmp/Nord.itermcolors
-elif [ -n "$XDG_CURRENT_DESKTOP" ]; then
-  cd $HOME/.nord-gnome-terminal/src && git pull && ./nord.sh
-fi
