65f8c23a4c2fc7ce937abc920426215958e4a811
Lin Yang <github@linyang.me>
2023-04-14 13:35:02 -0700

    Fix homebrew initial setup

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ setup-Darwin() {
   if [[ $(command -v brew) == "" ]]; then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   else
     brew update
   fi
