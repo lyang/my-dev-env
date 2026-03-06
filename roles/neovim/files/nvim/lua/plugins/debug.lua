9df1867f2733f95bbad803c88cc8868e6a351162
Lin Yang <github@linyang.me>
2026-03-05 22:33:29 -0800

    Add neotest, nvim-dap, and mini.surround (#33)
    neotest: Test runner with adapters for Python, Ruby (RSpec),
    Java, Rust, and JS/TS (Jest) under <leader>t prefix.
    
    nvim-dap + dap-ui: Debug Adapter Protocol client with
    auto-installed debuggers (Python, Rust/C, JS/TS) and
    auto-opening UI under <leader>d prefix.
    
    mini.surround: Lua-native surround replacing vim-surround
    and vim-repeat. Remapped to gz* prefix to avoid flash.nvim
    conflict.

---
+++ roles/neovim/files/nvim/lua/plugins/debug.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "jay-babu/mason-nvim-dap.nvim",
+      "leoluz/nvim-dap-go",
+      "mfussenegger/nvim-dap-python",
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step out" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run last" },
+      { "<leader>dx", function() require("dap").terminate() end, desc = "Terminate" },
+    },
+    config = function()
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "python", "codelldb", "js" },
+        automatic_installation = true,
+      })
+
+      -- Python: use debugpy from mason
+      local mason_path = vim.fn.stdpath("data") .. "/mason"
+      require("dap-python").setup(mason_path .. "/packages/debugpy/venv/bin/python")
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    keys = {
+      { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+    },
+    config = function()
+      local dapui = require("dapui")
+      dapui.setup()
+
+      -- Auto open/close UI on debug sessions
+      local dap = require("dap")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
--- roles/neovim/files/nvim/lua/plugins/editor.lua
+++ roles/neovim/files/nvim/lua/plugins/editor.lua
@@ -1,7 +1,21 @@
 return {
-  { "tpope/vim-surround" },
+  {
+    "echasnovski/mini.surround",
+    event = "VeryLazy",
+    opts = {
+      -- Remap to gz* to avoid conflict with flash.nvim's s key
+      mappings = {
+        add = "gza",
+        delete = "gzd",
+        find = "gzf",
+        find_left = "gzF",
+        highlight = "gzh",
+        replace = "gzr",
+        update_n_lines = "gzn",
+      },
+    },
+  },
   { "RRethy/nvim-treesitter-endwise", event = "InsertEnter" },
-  { "tpope/vim-repeat" },
   { "tpope/vim-rails", ft = "ruby" },
   { "tpope/vim-rake", ft = "ruby" },
   { "tpope/vim-projectionist" },
